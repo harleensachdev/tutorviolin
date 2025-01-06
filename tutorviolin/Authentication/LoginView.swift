087edb977867e256239e245d2c55ccba7d8a55c8
harleensachdev <blackberezz@gmail.com>
2025-01-06 21:36:32 +0800

    first commit

---
+++ tutorviolin/Authentication/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  tutorviolin
+//
+//  Created by caramelloveschicken on 06/01/2025.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
---
+++ tutorviolin/Components/Workspace/WorkspaceView.swift
@@ -0,0 +1,18 @@
+//
+//  WorkspaceView.swift
+//  tutorviolin
+//
+//  Created by caramelloveschicken on 06/01/2025.
+//
+
+import SwiftUI
+
+struct WorkspaceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WorkspaceView()
+}
